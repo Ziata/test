459a5c33421d3bdcb831eeb0ed3aa01ee1b9e6f9
FIRST_NAME LAST_NAME <gavriljuk.r.s@gmail.com>
2023-11-22 22:15:30 +0200

    add key

--- src/pages/[lang]/contact.tsx
+++ src/pages/[lang]/contact.tsx
@@ -164,7 +164,7 @@ const Contact: React.FC<PageProps> = ({
                 />
               </label>
               <div className="flex items-center">
-                <ReCAPTCHA sitekey="6Les_BgpAAAAAEAhl_wl6My9eN47WBQUVTHntaWr" />
+                <ReCAPTCHA sitekey="6Les_BgpAAAAAA1GKcqKQBCwRTEJgpIZHln5ro9z" />
               </div>
             </div>
             {statusMessage && (
