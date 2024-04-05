b0bb6ae3b7244804f10fb5b8a04b9a5e48e3e557
FIRST_NAME LAST_NAME <gavriljuk.r.s@gmail.com>
2024-04-05 19:12:25 +0300

    test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,8 +11,9 @@ import Search from "@/components/Search/Search";
 import { LayoutContext } from "@/context/LayoutContext";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
-export default function Header() {
+export default function Header({ metadata }: any) {
   const { headerData: data } = useContext(LayoutContext);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState<boolean>(false);
@@ -97,3 +98,18 @@ export default function Header() {
     </header>
   );
 }
+
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const metadata = {
+    title: "Заголовок страницы",
+    description: "Описание страницы",
+    image: "URL изображения",
+  };
+
+  return {
+    props: {
+      metadata,
+    },
+  };
+};
