997691194178608e812738ca35976919b30b525e
FIRST_NAME LAST_NAME <gavriljuk.r.s@gmail.com>
2024-04-05 21:40:28 +0300

    test

--- src/services/generateMeta.tsx
+++ src/services/generateMeta.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 
 interface Props {
-    title: string
+    title: string,
 }
 
 function HeadSEO(props: Props) {
     return <Head><title>{props.title}</title>
             <meta name="description" content={'rgerg'} />
-            <meta property="og:image" content={'egwreg'}/>
+            <meta property="og:image" content={'https://nextquestion.g-team.org/wp-content/uploads/2023/11/next_question_logo_h_w_cm.svg'}/>
             <link rel="icon" href={'efwegwerg'} />
     </Head>
 }
